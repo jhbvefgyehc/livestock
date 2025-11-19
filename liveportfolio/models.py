5417365c248bc9c78e0b304029f731bdab6d5eb9
jhbvefgyehc <sachinsubhash286@gmail.com>
2025-11-19 23:40:49 +0530

    Add admin config and update Portfolio model

--- liveportfolio/models.py
+++ liveportfolio/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Portfolio(models.Model):
-    stock_symbol = models.CharField(max_length=10)
-    quantity = models.IntegerField()
+    stock_symbol = models.CharField(max_length=20)  # 20+ chars for long BSE/NSE codes
+    quantity = models.PositiveIntegerField()
     buy_price = models.FloatField()
-    buy_date = models.DateField(auto_now_add=True)
+    buy_date = models.DateField()  # Let user/admin set purchase date
 
     def __str__(self):
         return f"{self.stock_symbol} ({self.quantity})"
