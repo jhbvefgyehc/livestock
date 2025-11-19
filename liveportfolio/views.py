498212c3a9917445ae3bdfcc1ad239d47ab9df4e
jhbvefgyehc <sachinsubhash286@gmail.com>
2025-11-19 22:54:07 +0530

    Add stock_price_view for Render deploy compatibility

--- liveportfolio/views.py
+++ liveportfolio/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Portfolio
-from .api_utils import get_stock_price
+import requests
+
+def get_stock_price(symbol):
+    url = 'https://www.alphavantage.co/query'
+    params = {
+        'function': 'GLOBAL_QUOTE',
+        'symbol': symbol,
+        'apikey': 'SZSRMMUNODCLYRUO'  # Use your actual Alpha Vantage API key
+    }
+    data = requests.get(url, params=params).json()
+    try:
+        price = data['Global Quote']['05. price']
+    except Exception:
+        price = "N/A"
+    return price
+
+def stock_price_view(request):
+    symbol = request.GET.get('symbol', 'TATASTEEL.BSE')
+    price = get_stock_price(symbol)
+    return HttpResponse(f"Current price for {symbol}: ₹{price}")
 
 def portfolio_dashboard(request):
     holdings = Portfolio.objects.all()
